2cccd3fab4f94f90f791621060a973ce4b6d17c9
unknown <muruganandhamk@PC1350.itp.objectfrontier.com>
2014-10-14 17:50:21 +0530

    Added ws folder and a file

--- com/ofs/training/git/HelloWorld.java
+++ com/ofs/training/git/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 
 	public static void main(String args[]){
 		System.out.println("Hello World");
+		System.out.println("Welcome to my first program in git");
 	}
 }
---
+++ com/ofs/training/jdbc/JDBCConnector.java
@@ -0,0 +1,6 @@
+package com.ofs.training.jdbc;
+
+public class JDBCConnector {
+public JDBCConnector(){
+}
+}
